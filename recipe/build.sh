e0fb87cc7a55b5577eedfc91e3da07581f1e1194
H. Vetinari <h.vetinari@gmx.com>
2023-03-16 00:35:48 +1100

    undo debug removal in build.sh
    Reinstate 28196b162f033296d154d8c666c39c6427707b14 since it doesn't hurt & may help

--- recipe/build.sh
+++ recipe/build.sh
@@ -54,5 +54,7 @@ pushd cctools_build_final
     --disable-static \
     --with-libtapi=${PREFIX} \
     --enable-shared || (cat config.log && cat config.status && false)
+  cat config.log
+  cat config.status
   make -j${CPU_COUNT} ${VERBOSE_AT}
 popd
