50a96c1edfbbe3882ee8fd3112d24f432ac9e042
Isuru Fernando <isuruf@gmail.com>
2025-12-16 14:39:39 -0800

    fix typo

--- recipe/install-cctools-symlinks.sh
+++ recipe/install-cctools-symlinks.sh
@@ -3,7 +3,7 @@ prefix="${cross_macos_machine}-"
 pushd $PREFIX/bin
   for tool in $(ls ${prefix}*); do
     # libtool conflicts with autotool's libtool
-    [[ "${tool}" == "${prefix}-libtool" ]] && continue;
+    [[ "${tool}" == "${prefix}libtool" ]] && continue;
     ln -s $PREFIX/bin/$tool $PREFIX/bin/${tool:${#prefix}} || true
   done
 popd
